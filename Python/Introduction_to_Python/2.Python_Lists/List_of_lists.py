1e0cd4b7176a18fc140d85b9cf54d29e324b7739
egor_follia <egorfollia@gmail.com>
2019-09-18 11:09:22 +0300

    Finished Python Lists

---
+++ Python/Introduction_to_Python/2.Python_Lists/List_of_lists.py
@@ -0,0 +1,29 @@
+'''
+As a data scientist, you'll often be dealing with a lot of data, and it will make sense to group some of this data.
+
+Instead of creating a flat list containing strings and floats, representing the names and areas of the rooms in your house, you can create a list of lists. The script on the right can already give you an idea.
+
+Don't get confused here: "hallway" is a string, while hall is a variable that represents the float 11.25 you specified earlier.
+
+'''
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
