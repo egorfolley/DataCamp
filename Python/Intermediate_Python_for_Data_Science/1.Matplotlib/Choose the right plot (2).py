4feafc885e9f1bb29bdf964644c62ebff9b23619
egor_follia <egorfollia@gmail.com>
2019-09-22 21:26:58 +0300

    Finished Matplot library

---
+++ Python/Intermediate_Python_for_Data_Science/1.Matplotlib/Choose the right plot (2).py
@@ -0,0 +1,7 @@
+'''
+You're a professor in Data Analytics with Python, and you want to visually assess if longer answers on exam questions lead to higher grades. Which plot do you use?
+
+'''
+
+# ANSWER
+# Scatter plot
